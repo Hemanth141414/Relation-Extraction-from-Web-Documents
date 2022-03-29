fff4a219b11a1e06de954ba78ab91723aeff8c31
hemanth <hemanthchandrapalle@gmail.com>
2022-03-29 23:42:32 +0000

    pushing secondary changes

--- ise/__main__.py
+++ ise/__main__.py
@@ -4,7 +4,9 @@ import urllib.error
 import http.client
 import json
 import ssl
+import spacy
 
+from spacy_help_functions import get_entities, create_entity_pairs
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 
@@ -13,6 +15,8 @@ from googleapiclient.discovery import build
 from config import DEVELOPER_KEY, SEARCH_ENGINE_ID
 
 def main():
+    raw_text = "Zuckerberg attended Harvard University, where he launched the Facebook social networking service from his dormitory room on February 4, 2004, with college roommates Eduardo Saverin, Andrew McCollum, Dustin Moskovitz, and Chris Hughes. Bill Gates stepped down as chairman of Microsoft in February 2014 and assumed a new post as technology adviser to support the newly appointed CEO Satya Nadella. "
+    entities_of_interest = ["ORGANIZATION", "PERSON", "LOCATION", "CITY", "STATE_OR_PROVINCE", "COUNTRY"]
     r = sys.argv[1]
     t = sys.argv[2]
     q = sys.argv[3]
@@ -20,10 +24,16 @@ def main():
     googleResults = googleQueryAPI(q)
     addContent(q, googleResults)
     print("Query results are: \n \n \n")
-    for i in googleResults:
-        print(i)
-        print("*********************************************************************************** \n")
-	
+    nlp = spacy.load("en_core_web_lg")
+
+    doc = nlp(raw_text)
+    for sentence in doc.sents:
+        print("\n\nProcessing entence: {}".format(sentence))
+        print("Tokenized sentence: {}".format([token.text for token in sentence]))
+        ents = get_entities(sentence, entities_of_interest)
+        print("spaCy extracted entities: {}".format(ents))
+
+        
 def googleQueryAPI(query):
     service = build("customsearch", "v1", developerKey=DEVELOPER_KEY)
     res = service.cse().list(
